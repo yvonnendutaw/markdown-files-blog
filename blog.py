775401b188e72520fbf6c80815e2715f528026e3
yvonnendutaw <yvonita7@gmail.com>
2016-06-12 09:16:11 +0300

    added file uploader

--- blog.py
+++ blog.py
@@ -1,7 +1,9 @@
+import os
 import sys
-from flask import Flask, render_template
+from flask import Flask, render_template,  request, redirect, url_for, send_from_directory
 from flask_flatpages import FlatPages, pygments_style_defs
 from flask_frozen import Freezer
+from werkzeug import secure_filename
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -13,6 +15,12 @@ app = Flask(__name__)
 flatpages = FlatPages(app)
 freezer = Freezer(app)
 app.config.from_object(__name__)
+
+
+# This is the path to the upload directory
+app.config['UPLOAD_FOLDER'] = 'content/posts/'
+# These are the extension that we are accepting to be uploaded
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'md', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
 @app.route("/")
@@ -27,6 +35,44 @@ def post(name):
     path = '{}/{}'.format(POST_DIR, name)
     post = flatpages.get_or_404(path)
     return render_template('post.html', post=post)
+
+# For a given file, return whether it's an allowed type or not
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+# This route will show a form to perform an AJAX request
+# jQuery is loaded to execute the request and update the
+# value of the operation
+
+# Route that will process the file upload
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded file
+    file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+	posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
+	posts.sort(key=lambda item:item['date'], reverse=False)
+	return render_template('posts.html', posts=posts)
+
+# This route is expecting a parameter containing the name
+# of a file. Then it will locate that file on the upload
+# directory and show it on the browser, so if the user uploads
+# an image, that image is going to be show after the upload
+@app.route('/posts/<filename>')
+def uploaded_file(filename):
+    posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
+    posts.sort(key=lambda item:item['date'], reverse=False)
+    return render_template('posts.html', posts=posts)
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "build":
